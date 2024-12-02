d7e8950311f1be3bede332f4c6cb266f27300926
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-12-02 11:18:56 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/talesnunes/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.talesnunes.hrworker.repositories;
+
+import com.talesnunes.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
