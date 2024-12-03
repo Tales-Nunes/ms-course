0184ed5acab82e1cde006e4f202d51afc0f2c184
Tales-Nunes <talesnunes.siqueira@gmail.com>
2024-12-03 10:45:10 -0300

    configuring eureka server

--- hr-eureka-server/src/main/java/com/talesnunes/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/talesnunes/hreurekaserver/HrEurekaServerApplication.java
@@ -2,6 +2,9 @@ package com.talesnunes.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
 
 @SpringBootApplication
 public class HrEurekaServerApplication {
